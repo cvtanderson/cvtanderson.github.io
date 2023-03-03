c9cdcc998c3663246a6608e7d2195bfc33deb06d
cvtanderson <123899162+cvtanderson@users.noreply.github.com>
2023-03-03 10:51:35 -0300

    Add files via upload

---
+++ link.js
@@ -0,0 +1,13 @@
+function link_exp(){
+    location.href = "info_Exp.html"
+}
+
+function link_cono(){
+    location.href = "info_cono.html"
+
+}
+
+function link_edu(){
+    location.href = "info_edu.html"
+
+}
